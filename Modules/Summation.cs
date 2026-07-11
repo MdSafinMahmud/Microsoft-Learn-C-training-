5a76fb4d5139f560e8d06b22b60b662028041bc0
Md. Safin Mahmud <m.safin.mahmud@gmail.com>
2026-07-11 20:00:29 +0600

    Create Summation.cs

---
+++ Modules/Summation.cs
@@ -0,0 +1,7 @@
+int firstNum = 12;
+int secondNumber = 7;
+Console.WriteLine(firstNum + secondNumber);
+
+string firstName = "Safin";
+int widgetsSold = 7;
+Console.WriteLine($"{firstName} sold {widgetsSold} widgets!");
