df3c89612327a5351a8e163e1b028a1cd703fa1f
Md. Safin Mahmud <m.safin.mahmud@gmail.com>
2026-07-11 18:27:36 +0600

    c#

---
+++ Module_1/Display_literal_and_variable_values.cs
@@ -0,0 +1,7 @@
+string firstName = "Bob";
+int num = 3;
+float num1 = 34.4F;
+Console.WriteLine($"Hello,{firstName}! You have {num} messeage in your inbox.The temperature is {num1} celcius.");
+Console.WriteLine(34.40M);
+decimal x = 12.3m;
+Console.WriteLine(x);
