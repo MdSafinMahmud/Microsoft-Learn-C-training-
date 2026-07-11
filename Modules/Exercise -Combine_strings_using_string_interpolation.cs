5d2b52dcf3dd9dc07f892b03a069b31f800950e7
Md. Safin Mahmud <m.safin.mahmud@gmail.com>
2026-07-11 19:46:04 +0600

    c#

---
+++ Modules/Exercise -Combine_strings_using_string_interpolation.cs
@@ -0,0 +1,11 @@
+string firstName = "Safin";
+string message = $"Hello {firstName}!";
+Console.WriteLine(message);
+
+int version = 11;
+string updateText = "Update to Windows";
+string message1 = $"{updateText} {version}";
+Console.WriteLine(message1);
+
+string projectName = "First-Project";
+Console.WriteLine($@"C:\Output\{projectName}\Data");
---
+++ Modules/practice.cs
@@ -0,0 +1,6 @@
+string firstName = "Safin";
+string message = "Hello " + firstName+"!";
+Console.WriteLine(message);
+string greeting = "Hello!";
+string message1 = greeting + "" + firstName + "!";
+Console.WriteLine(message1);
