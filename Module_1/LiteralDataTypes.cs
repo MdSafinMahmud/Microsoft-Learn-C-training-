e7be8804c2450e98b8d23cd0e505ee31876bf7aa
Md. Safin Mahmud <m.safin.mahmud@gmail.com>
2026-07-11 17:41:42 +0600

    c#

---
+++ Module_1/LiteralDataTypes.cs
@@ -0,0 +1,20 @@
+Console.WriteLine('b');//it's correct.
+// Console.WriteLine('Hello world!');//its incorrect.
+
+Console.WriteLine(123);//it's correct.
+Console.WriteLine(0.25F);
+Console.WriteLine(2.625);
+Console.WriteLine(12.39816m);
+
+// Use Boolean literals
+Console.WriteLine(true);
+Console.WriteLine(false);
+
+// Output: 123
+Console.WriteLine("123");
+// Output: 123
+Console.WriteLine(123);
+// Output: true
+Console.WriteLine("true");
+// Output: True
+Console.WriteLine(true);
