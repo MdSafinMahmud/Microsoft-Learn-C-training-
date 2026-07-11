4a09820ad0f418ba86e1505b96e84f4345699ec3
Md. Safin Mahmud <m.safin.mahmud@gmail.com>
2026-07-11 19:04:46 +0600

    Create CharacterSequences.cs

---
+++ Modules/CharacterSequences.cs
@@ -0,0 +1,13 @@
+Console.WriteLine("Hello\nWorld!");
+Console.WriteLine("Hello\tWorld!");
+Console.WriteLine("Hello \"World\"!");
+// Console.WriteLine("c:\source\repos");
+Console.WriteLine("c:\\source\\repos");
+
+Console.WriteLine("Generating invoices for customers\"Contoso Corp\"...\n");
+Console.WriteLine("Invoice:1021\t\tComplete!");
+Console.WriteLine("Invoice:1022\t\tComplete!");
+Console.Write("\nOutput Directory:\t");
+// Console.WriteLine(@"    c:\source\repos    
+//         (this is where your code goes)");
+Console.Write(@"c:\invoices");
