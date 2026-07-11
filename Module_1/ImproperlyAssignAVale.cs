d5492fa77bb1b2a8b2cedc77dc33e26e1bf820a2
Md. Safin Mahmud <m.safin.mahmud@gmail.com>
2026-07-11 18:04:06 +0600

    c#

---
+++ Module_1/ImproperlyAssignAVale.cs
@@ -0,0 +1,4 @@
+int firstName;
+firstName = "Bob";
+Console.WriteLine(firstName);
+// error CS0029: Cannot implicitly convert type 'string' to 'int'
---
+++ Module_1/ReassignTheValueofAVariable.cs
@@ -0,0 +1,17 @@
+string firstName;
+firstName = "Safin";
+Console.WriteLine(firstName);
+firstName = "Mahmud";
+Console.WriteLine(firstName);
+firstName = "Liem";
+Console.WriteLine(firstName);
+firstName = "Isabella";
+Console.WriteLine(firstName);
+firstName = "Yasmin";
+Console.WriteLine(firstName);
+
+
+// string firstName;
+// Console.WriteLine(firstName);
+// it's not correct code.
+// error CS0165: Use of unassigned local variable 'firstName'
