2518def92e2ac9bf5c93b59c5d3cc1e0baa908a0
Md. Safin Mahmud <m.safin.mahmud@gmail.com>
2026-07-11 17:56:09 +0600

    Update ExerciseVariable.cs

--- Module_1/ExerciseVariable.cs
+++ Module_1/ExerciseVariable.cs
@@ -5,5 +5,6 @@ Console.Write($"Hi,{firstName}");
 //both are not equal.
 
 // string firstName;
-// "Mahmud" = firstName;
+// "Safin" = firstName;
 // Console.Write($"Hi,{firstName}");
+//error return is : CS0131: The left-hand side of an assignment must be a variable, property or indexer
