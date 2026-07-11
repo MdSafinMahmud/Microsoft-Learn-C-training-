61c99f5a4ca184856ea778dde11e57be6fe55ff5
Md. Safin Mahmud <m.safin.mahmud@gmail.com>
2026-07-11 20:31:32 +0600

    Update MathOperation.cs

--- Modules/MathOperation.cs
+++ Modules/MathOperation.cs
@@ -1,5 +1,4 @@
 using System.Text;
-
 int sum = 7 + 5;
 int difference = 7 - 5;
 int product = 7 * 5;
@@ -25,7 +24,36 @@ Console.WriteLine(decimalQuotient2 + decimalQuotient1);
 // decimal decimalQuotientD = 7 / 5;
 // Console.WriteLine(decimalQuotientA + decimalQuotientB + decimalQuotientC + decimalQuotientD);
 
+// decimal numbers:
 int first = 7;
 int second = 5;
 decimal quotient = (decimal)first / (decimal)second;
 Console.WriteLine(quotient);
+
+// Modulus:
+Console.WriteLine($"Modulus of 200/5: {200 / 5}");
+Console.WriteLine($"Modulus of 7/5: {7 / 5}");
+// In math, PEMDAS is an acronym that helps students remember the order of operations. The order is:
+
+//1.Parentheses(whatever is inside the parenthesis is performed first)
+//2.Exponents
+//3.Multiplication and Division (from left to right)
+//4.Addition and Subtraction (from left to right)
+
+// Write code to exercise C#'s order of operations
+int value1 = 3 + 4 * 5;
+int value2 = (3 + 4) * 5;
+Console.WriteLine(value1);
+Console.WriteLine(value2);
+
+// same same but different.
+int value = 0;     // value is now 0.
+value = value + 5; // value is now 5.
+value += 5;
+Console.WriteLine(value); // value is now 10.
+
+// The++ operator increments the value of the variable by 1
+int value3 = 0;     // value is now 0.
+value3 = value3 + 1; // value is now 1.
+value3++; // value is now 2.
+Console.WriteLine(value3);
