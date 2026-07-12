be48b50477ac3ca9d5abf61b710470223d1a2ca9
Md. Safin Mahmud <m.safin.mahmud@gmail.com>
2026-07-12 22:08:47 +0600

    c#

---
+++ Modules/Store_grade_values.cs
@@ -0,0 +1,19 @@
+int gradeA = 4;
+int gradeB = 3;
+Console.WriteLine("Course\t\t\tGrade");
+string course1Name = "English 101";
+string course2Name = "Algebra 101";
+string course3Name = "Biology 101";
+string course4Name = "Computer Science I";
+string course5Name = "Psychology 101";
+int course1Grade = gradeA;
+int course2Grade = gradeB;
+int course3Grade = gradeB;
+int course4Grade = gradeB;
+int course5Grade = gradeA;
+Console.WriteLine($"{course1Name}\t\t{course1Grade}");
+Console.WriteLine($"{course2Name}\t\t{course2Grade}");
+Console.WriteLine($"{course3Name}\t\t{course3Grade}");
+Console.WriteLine($"{course4Name}\t{course4Grade}");
+Console.WriteLine($"{course5Name}\t\t{course5Grade}");
+
